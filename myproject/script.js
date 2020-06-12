821c543272a26651b1cbd0402368d184e366962e
rcassiohub <raphael.duro@bandtec.com.br>
2020-06-11 23:09:56 -0300

    home e contato atualizado, e add o simulador financeiro

--- myproject/script.js
+++ myproject/script.js
@@ -1,12 +1,14 @@
 function comprar(){
-    let PERA = 10*(qtd_pera);
-    let MACA = 20*(qtd_maca);
-    let CEREJA = 50*(qtd_cereja);
-    let KIWI = 25*(qtd_kiwi); 
-    let ValorTotal = PERA + MACA + CEREJA + KIWI
+    let PERA = (qtd_pera.value)*20;
+    let MACA = (qtd_maca.value)*25;
+    let CEREJA = (qtd_cereja.value)*50;
+    let KIWI = (qtd_kiwi.value)*30; 
+    let ValorTotal = PERA + MACA + CEREJA + KIWI;
      
     if(ValorTotal > 0){
-        resultado.innerHTML = `O valor total da sua compra foi de: R$${ValorTotal},00`
+        img_carrinho.src ="../imagens/vendas.png" ;
+        resultado_compra.innerHTML = "Sua compra foi realizada com sucesso!";
+        resultado.innerHTML = `O valor total da sua compra foi de: R$${ValorTotal},00`;
         // total_compra.src = '../imagens/escoar.png'
     } 
 }
--- myproject/script_simu.js
+++ myproject/script_simu.js
@@ -1,57 +1,57 @@
-window.addEventListener("load", start);
-let pera = document.querySelector("#pera");
-let maca = document.querySelector("#maca");
-let kiwi = document.querySelector("#kiwi");
-let cereja = document.querySelector("#cereja");
+// window.addEventListener("load", start);
+// let pera = document.querySelector("#pera");
+// let maca = document.querySelector("#maca");
+// let kiwi = document.querySelector("#kiwi");
+// let cereja = document.querySelector("#cereja");
 
-let inputPera = document.querySelector("#input_pera");
-let inputMaca = document.querySelector("#input_maca");
-let inputKiwi = document.querySelector("#input_kiwi");
-let inputCereja = document.querySelector("#input_cereja");
+// let inputPera = document.querySelector("#input_pera");
+// let inputMaca = document.querySelector("#input_maca");
+// let inputKiwi = document.querySelector("#input_kiwi");
+// let inputCereja = document.querySelector("#input_cereja");
 
-let totalCompraDiv = document.querySelector(".total_compra");
+// let totalCompraDiv = document.querySelector(".total_compra");
 
-function render() {
-  pera.addEventListener("change", function (event) {
-    colocarValorNoInput(inputPera, event.target.value);
-    totalCompraDiv.innerHTML = `
-    <h2> R$${comprar()},00 </h2>
-    `;
-  });
-  maca.addEventListener("change", function (event) {
-    colocarValorNoInput(inputMaca, event.target.value);
-    totalCompraDiv.innerHTML = `
-    <h2> R$${comprar()},00 </h2>
-    `;
-  });
-  kiwi.addEventListener("change", function (event) {
-    colocarValorNoInput(inputKiwi, event.target.value);
-    totalCompraDiv.innerHTML = `
-    <h2> R$${comprar()},00 </h2>
-    `;
-  });
-  cereja.addEventListener("change", function (event) {
-    colocarValorNoInput(inputCereja, event.target.value);
-    totalCompraDiv.innerHTML = `
-    <h2> R$${comprar()},00 </h2>
-    `;
-  });
-}
+// function render() {
+//   pera.addEventListener("change", function (event) {
+//     colocarValorNoInput(inputPera, event.target.value);
+//     totalCompraDiv.innerHTML = `
+//     <h2> R$${comprar()},00 </h2>
+//     `;
+//   });
+//   maca.addEventListener("change", function (event) {
+//     colocarValorNoInput(inputMaca, event.target.value);
+//     totalCompraDiv.innerHTML = `
+//     <h2> R$${comprar()},00 </h2>
+//     `;
+//   });
+//   kiwi.addEventListener("change", function (event) {
+//     colocarValorNoInput(inputKiwi, event.target.value);
+//     totalCompraDiv.innerHTML = `
+//     <h2> R$${comprar()},00 </h2>
+//     `;
+//   });
+//   cereja.addEventListener("change", function (event) {
+//     colocarValorNoInput(inputCereja, event.target.value);
+//     totalCompraDiv.innerHTML = `
+//     <h2> R$${comprar()},00 </h2>
+//     `;
+//   });
+// }
 
-function colocarValorNoInput(inputFruta, valor) {
-  inputFruta.value = valor;
-}
-function comprar() {
-  let valorTotalPera = 10 * pera.value;
-  let valorTotalMaca = 15 * maca.value;
-  let valorTotalKiwi = 20 * kiwi.value;
-  let valorTotalCereja = 50 * cereja.value;
-  let totalCompra =
-    valorTotalPera + valorTotalMaca + valorTotalKiwi + valorTotalCereja;
+// function colocarValorNoInput(inputFruta, valor) {
+//   inputFruta.value = valor;
+// }
+// function comprar() {
+//   let valorTotalPera = 10 * pera.value;
+//   let valorTotalMaca = 15 * maca.value;
+//   let valorTotalKiwi = 20 * kiwi.value;
+//   let valorTotalCereja = 50 * cereja.value;
+//   let totalCompra =
+//     valorTotalPera + valorTotalMaca + valorTotalKiwi + valorTotalCereja;
 
-  return totalCompra;
-}
+//   return totalCompra;
+// }
 
-function start() {
-  render();
-}
+// function start() {
+//   render();
+// }
