cbc095c975a52a9501bf64ed8fd126fd6fc4d393
rcassiohub <raphael.duro@bandtec.com.br>
2020-06-18 03:36:54 -0300

    implementação das restrições na area de contato e animação na home

---
+++ projeto-site/public/index.js
@@ -0,0 +1,40 @@
+function enviaContato() {
+    var erros = validaContato();
+    mensagemErro.innerHTML = '';
+
+    if (erros.length > 0) {
+        for (var i = 0; i < erros.length; i++) {
+            var erro = erros[i]; // percorrendo os TODOS os ERROS       
+            var li = document.createElement("li"); //Cria um elemento do HTML no JS
+            li.innerHTML = erro;
+
+            mensagemErro.appendChild(li); // vai dar um "filho" para o "mensagemErro" 
+        }
+    }
+}
+
+
+function validaContato() {
+    var erros = []; // vetor "armazena" vários valores em um nome, permitindo a percorrência desses valores 
+    if (!nome_contato.value) {
+        erros.push("Preencha o nome"); //Empurra esta informação dentro do vetor "erros"
+    }
+    if ((email_contato.value.search("@") == -1) ||
+        (email_contato.value.search(".") == -1) ||
+        (email_contato.value.search(" ") >= 1) ||
+        email_contato.value[email_contato.length - 3] != ".com") {
+        erros.push("O formato de e-mail é: usuario@dominio.com");
+
+    }
+    if (!mensagem_contato.value) {
+        erros.push("Preencha a mensagem!")
+    }
+    if ((tel_contato.value.search("-") == -1) ||
+        tel_contato.value[tel_contato.value.length - 5] != "-"
+    ) {
+        erros.push("O formato do telefone é: 0090000-0000");
+    }
+    return erros;
+}
+
+
